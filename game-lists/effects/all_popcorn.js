6f356df58d453d83ede862d2534ed4c71d3a51fc
MysteriousGrimReaper <hitherehangouts@gmail.com>
2024-04-12 22:00:01 -0700

    popcorn

---
+++ game-lists/effects/all_popcorn.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: `pa`,
+	text: `Popcorn for All`,
+	async effect({ uno_players }) {
+		uno_players.forEach((player) => player.popcorn++)
+        await uno_players.game_channel.send(`Everyone got a fresh bucket of popcorn!`)
+	},
+};
---
+++ game-lists/effects/popcorn.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: `po`,
+	text: `Popcorn`,
+	async effect({ uno_players, player }) {
+		player.popcorn++
+        await uno_players.game_channel.send(`${player.user} got a fresh bucket of popcorn! @silent`)
+	},
+};
